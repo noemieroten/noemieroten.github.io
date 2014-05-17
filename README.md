e3673ff27eb319675a4c1e17631d7f051dff946b
noeeee <noemieroten@hotmail.com>
2014-05-18 01:57:01 +0200

    update david

--- README.md
+++ README.md
@@ -19,7 +19,22 @@ The csv has the following columns:
 
 # Assumptions about the inputs
 
-As previously noted the pictures for each _location_ __loc__are located in the folder _data/pictures/**loc**_. They are nammed as __xx.jpg__ where _xx_ starts from 01 up to the number given by _nPictures_.
+The pictures should be *.jpg* files in 4/3 format (with resolution for example _640x480_).
 
+As previously noted the pictures for each _location_ __loc__ are located in the folder _data/pictures/**loc**_. They are nammed as __xx.jpg__ where _xx_ starts from 01 up to the number given by _nPictures_.
+
+For example, to continue our example about New York, the folder __data/pictures/New York__ should contain the jpegs __01.jpg__, __02.jpg__, ..., up to nPictures.
 
 # How it works
+
+It is mainly backend by *topoJson*, *D3.js* and *flexslider*. The scripts first draws the maps, then the voronoi partition (such that the user's mouse do not to be perfectly on the city to be able to click), add the cities to the map, and then add the interaction with the dom.
+
+# Idea on how to improve the project
+
+1. One should definitively be able to remove the nPictures column from the location.csv. Using python would be a simply task, but my limitation of javascript are so I could not do it. The scripts should also be fine with any file name in the __data/pictures/location__ . It should also accept any kind of picture considered as valid by the _img_ html tag.
+
+2. Any resolution of images should be accepted. I had some issue with the resizing of the flexslider.
+
+3. Nowadays, most pictures contains *GPS* location information. Hence we could be able to use them to cluster the picture (using a simple hierarical tree) and then guessing the location from the inferred clusters (by taking the mean of the coordinates and trying to find the closest city). This would remove from the user the hurdle to type the location and to look for the coordinates of that location.
+
+4. Design could definitievly improved, although this should according everyone's tastes.
--- scripts/world_map.js
+++ scripts/world_map.js
@@ -1,41 +1,43 @@
-var width = 630,
+var width = 580,
 height = 480;
 
-var projection = d3.geo.mercator()
-    .scale((width + 1) / 1.6 / Math.PI)
-    .translate([width / 2 - 25, height / 2 + 80])
-    .precision(.1);
+
+//d3.geo.azimuthalEqualArea()
+var projection = d3.geo.mercator() 
+  .scale((width + 1) / 1.65 / Math.PI)
+  .translate([width / 2 - 25, height / 2 + 80])
+  .precision(.1);
 
 var path = d3.geo.path()
-    .projection(projection);
+  .projection(projection);
 
 var graticule = d3.geo.graticule();
 
 // Zoom hack
 var zoom = d3.behavior.zoom()
-    .scaleExtent([0.5, 10])
-    .on("zoom", zoomed);
+  .scaleExtent([0.5, 10])
+  .on("zoom", zoomed);
 
 var map = d3.select('#worldMap')
-    .attr("width", width)
-    .attr("height", height)
-    .call(zoom);
+  .attr("width", width)
+  .attr("height", height)
+  .call(zoom);
 
 var svg = map.append("g");
 
 // End of Zoom
 function zoomed() {
-    svg.attr("transform", "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")");
+  svg.attr("transform", "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")");
 }
 
 svg.append("path")
-    .datum(graticule)
-    .attr("class", "graticule")
-    .attr("d", path);
+  .datum(graticule)
+  .attr("class", "graticule")
+  .attr("d", path);
 
 queue()
-    .defer(d3.json, "data/world-110m.json")
-    .await(createMap);
+  .defer(d3.json, "data/world-110m.json")
+  .await(createMap);
 
 d3.csv('data/location.csv', putCityVoronoi)
 
@@ -43,147 +45,147 @@ d3.csv('data/location.csv', putCityVoronoi)
 
 function createMap(error, world) {
 
-    svg.insert("path", ".graticule")
-        .datum(topojson.feature(world, world.objects.land))
-        .attr("class", "land")
-        .attr("d", path);
+  svg.insert("path", ".graticule")
+    .datum(topojson.feature(world, world.objects.land))
+    .attr("class", "land")
+    .attr("d", path);
 
 }
 
 // Put dots on the map for the location from a csv file 
 // location, long, lat, sizeBubble, nPictures
 function putCityVoronoi(error, data){
-    
-    var centroid = d3.geo.centroid;
+  
+  var centroid = d3.geo.centroid;
 
-    data.forEach(function(d){
-        d.longitude = +d.longitude;
-        d.latitude = +d.latitude;
-        d.geojson = {type: 'Point', 
-                     coordinates: [d.latitude, d.longitude]};
+  data.forEach(function(d){
+    d.longitude = +d.longitude;
+    d.latitude = +d.latitude;
+    d.geojson = {type: 'Point', 
+                 coordinates: [d.latitude, d.longitude]};
+  });
+  
+  //Color scale
+  var color = d3.scale.quantile()
+    .range(d3.range(10).map(function(i) { j = i; return "q" + j + "-10"; }))
+    .domain([0, 1]);
+
+  //Voronoi tessleation
+  var voronoi = d3.geom.voronoi()
+    .clipExtent([[0,0], [width, height]]);
+
+  //Add a circle in the middle of the municipalities
+  var pathCentroid = function(d, i){
+    return path.centroid(d.geojson);
+  }
+
+  
+  // Voronoi tesslation
+
+  //Creates a voronois tesselation
+  function polygon(d) {
+    return "M" + d.join("L") + "Z";
+  }
+  
+  positions = data.map(function(d){
+    return pathCentroid(d);
+  })
+
+  polygons = d3.geom.voronoi(positions);
+
+  var voronoiArea = svg.append("g")
+    .attr("class", "municipality-voronoi")
+  // .attr("transform", 'translate(-' + translate_width_constant + ',0)')
+    .selectAll("path")
+    .data(polygons, polygon) //apply the polygon function to the dat polygon
+    .enter().append("path")
+    .attr("d", polygon)
+    .attr("stroke-width", "0")
+    .on("click", updateSlider);
+  
+  var tooltip = d3.select("body")
+	  .append("div")
+    .attr("class", "d3-tip worldMapTip")
+	  .style("position", "absolute")
+	  .style("z-index", "10")
+	  .style("visibility", "hidden");
+  
+  function htmlInsideTooltipFn(d, i){
+    d = data[i];
+    htmlText = "<div align='center'>";
+    htmlText += '<font color="red">' + d.location + "</font> ";
+    htmlText += '   (' + d.visitedDate + ')';
+    htmlText += '</div>';
+    // Add something here (Such as descriptions or date of tripe)  
+    return htmlText;
+  }
+  addToolTip(voronoiArea, tooltip, htmlInsideTooltipFn);
+  
+  //Add Circle to cities
+  var cities = svg.append("g")
+    .selectAll("circle")
+    .data(data)
+    .enter().append("circle")
+    .attr("class", "city")
+    .attr('r', function(d) {return 0.22*(d.bubbleSize+1);})
+    .attr("cx", function(d) { return pathCentroid(d)[0]; })
+    .attr("cy", function(d) { return pathCentroid(d)[1]; })
+    .on("click", updateSlider);
+
+  addToolTip(cities, tooltip, htmlInsideTooltipFn);
+  
+  var this_location;
+  function updateSlider(d, i){
+    
+    d = data[i];
+    if (this_location == d.location){
+      return;
+    }
+    console.log(this_location);
+    this_location = d.location;
+    var newSlides = updatePictures(d.location, d.nPictures);
+    d3.select('#pictureSlider')
+      .remove();
+    
+    d3.select('#sliderDiv')
+      .append('div')
+      .attr('class', 'flexslider')      
+      .attr('id', 'pictureSlider')
+      .style('width', '640px')
+      .style('height', '480px')
+      .html(newSlides);
+    
+    $('.flexslider').flexslider({
+      animation: "slide",
+      slideshowSpeed: 3000,
     });
+  }
+  function updatePictures(location, nPictures){
+    var newPict = function(n){            
+      return '<li> \n  <img src="data/pictures/' + 
+        location + '/' + (n < 10 ? '0' + n : n) + 
+        '.jpg" /> \n </li> \n';
+    };
     
-    //Color scale
-    var color = d3.scale.quantile()
-        .range(d3.range(10).map(function(i) { j = i; return "q" + j + "-10"; }))
-        .domain([0, 1]);
-
-    //Voronoi tessleation
-    var voronoi = d3.geom.voronoi()
-        .clipExtent([[0,0], [width, height]]);
-
-    //Add a circle in the middle of the municipalities
-    var pathCentroid = function(d, i){
-        return path.centroid(d.geojson);
+    htmlText = '<ul class="slides"> \n';
+    for (var i=1; i <= nPictures; ++i){
+      htmlText += newPict(i);
     }
-
-    
-    // Voronoi tesslation
-
-    //Creates a voronois tesselation
-    function polygon(d) {
-        return "M" + d.join("L") + "Z";
-    }
-    
-    positions = data.map(function(d){
-        return pathCentroid(d);
-    })
-
-    polygons = d3.geom.voronoi(positions);
-
-    var voronoiArea = svg.append("g")
-        .attr("class", "municipality-voronoi")
-    // .attr("transform", 'translate(-' + translate_width_constant + ',0)')
-        .selectAll("path")
-        .data(polygons, polygon) //apply the polygon function to the dat polygon
-        .enter().append("path")
-        .attr("d", polygon)
-        .attr("stroke-width", "0")
-        .on("click", updateSlider);
-    
-    var tooltip = d3.select("body")
-	    .append("div")
-        .attr("class", "d3-tip worldMapTip")
-	    .style("position", "absolute")
-	    .style("z-index", "10")
-	    .style("visibility", "hidden");
-    
-    function htmlInsideTooltipFn(d, i){
-        d = data[i];
-        htmlText = "<div align='center'>";
-        htmlText += '<font color="red">' + d.location + "</font> ";
-        htmlText += '   (' + d.visitedDate + ')';
-        htmlText += '</div>';
-        // Add something here (Such as descriptions or date of tripe)  
-        return htmlText;
-    }
-    addToolTip(voronoiArea, tooltip, htmlInsideTooltipFn);
-    
-    //Add Circle to cities
-    var cities = svg.append("g")
-        .selectAll("circle")
-        .data(data)
-        .enter().append("circle")
-        .attr("class", "city")
-        .attr('r', function(d) {return 0.25*(d.bubbleSize+1);})
-        .attr("cx", function(d) { return pathCentroid(d)[0]; })
-        .attr("cy", function(d) { return pathCentroid(d)[1]; })
-        .on("click", updateSlider);
-
-    addToolTip(cities, tooltip, htmlInsideTooltipFn);
-    
-    var this_location;
-    function updateSlider(d, i){
-        
-        d = data[i];
-        if (this_location == d.location){
-            return;
-        }
-        console.log(this_location);
-        this_location = d.location;
-        var newSlides = updatePictures(d.location, d.nPictures);
-        d3.select('#pictureSlider')
-            .remove();
-        
-        d3.select('#sliderDiv')
-            .append('div')
-            .attr('class', 'flexslider')      
-            .attr('id', 'pictureSlider')
-            .style('width', '640px')
-            .style('height', '480px')
-            .html(newSlides);
-        
-        $('.flexslider').flexslider({
-            animation: "slide",
-            slideshowSpeed: 3000,
-        });
-}
-    function updatePictures(location, nPictures){
-        var newPict = function(n){            
-            return '<li> \n  <img src="data/pictures/' + 
-                location + '/' + (n < 10 ? '0' + n : n) + 
-                '.jpg" /> \n </li> \n';
-        };
-        
-        htmlText = '<ul class="slides"> \n';
-        for (var i=1; i <= nPictures; ++i){
-            htmlText += newPict(i);
-        }
-        htmlText += '</ul> \n';
-        //console.log(htmlText);
-        return htmlText;
-    }
+    htmlText += '</ul> \n';
+    //console.log(htmlText);
+    return htmlText;
+  }
 
 }
 
 function addToolTip(g, tooltip, htmlInsideTooltipFn){
-    g.on("mouseover", function(d, i){
-        tooltip
-            .html(htmlInsideTooltipFn(d,i))
-            .style("visibility", "visible")
-        return true;})
-	    .on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
-	    .on("mouseout", function(){return tooltip.style("visibility", "hidden");})
+  g.on("mouseover", function(d, i){
+    tooltip
+      .html(htmlInsideTooltipFn(d,i))
+      .style("visibility", "visible")
+    return true;})
+	  .on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
+	  .on("mouseout", function(){return tooltip.style("visibility", "hidden");})
 }
 
 d3.select(self.frameElement).style("height", height + "px");
